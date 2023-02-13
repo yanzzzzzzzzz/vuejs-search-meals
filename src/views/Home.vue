c78a622ad642c18515dc42a4e020297d06503031
Jack Tsai <jtsai@nexify.com.hk>
2023-02-13 17:51:26 +0800

    install vuex and create store

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200"
+      placeholder="search for Meals"
+    />
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+</script>
