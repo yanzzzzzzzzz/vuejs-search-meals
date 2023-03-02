73ed9eaaa6d3b09a9014bae7e0ee0f93e1042a44
Jack Tsai <jtsai@nexify.com.hk>
2023-03-02 17:55:07 +0800

    build meal detail page

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <pre>{{ meal }}</pre>
+    <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" />
+    <div class="grid grid-cols-1 md:grid-cols-3">
+      <div>
+        <strong class="font-bold">Category:</strong>{{ meal.strCategory }}
+      </div>
+      <div><strong class="font-bold">Area:</strong>{{ meal.strArea }}</div>
+      <div><strong class="font-bold">Tags:</strong>{{ meal.strTags }}</div>
+    </div>
   </div>
 </template>
 <script setup>
@@ -11,7 +20,11 @@ const meal = ref({});
 const route = useRoute();
 onMounted(async () => {
   console.log("id", route.params.id);
-  const response = await axiosClient.get(`/lookup.php?i=${route.params.id}`);
+  const response = await axiosClient
+    .get(`/lookup.php?i=${route.params.id}`)
+    .then(({ data }) => {
+      meal.value = data.meals[0] || {};
+    });
   console.log("meal", response.data.meals);
 });
 </script>
