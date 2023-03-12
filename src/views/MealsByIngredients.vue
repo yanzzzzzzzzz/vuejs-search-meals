0ede0bc24f8bc007e38436029619e41c708e84ae
Jack Tsai <jtsai@nexify.com.hk>
2023-03-12 11:35:50 +0800

    MealsByIngredients

--- src/views/MealsByIngredients.vue
+++ src/views/MealsByIngredients.vue
@@ -1,4 +1,27 @@
 <template>
-  <div>By Ingredients</div>
+  <div class="p-8">
+    <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+    <router-link
+      v-for="ingredient of ingredients"
+      :key="ingredient.id"
+      class="bg-white rounded p-3 mb-3 shadow"
+      :to="{
+        name: 'byIngredient',
+        param: { ingredient: ingredient.idIngredient },
+      }"
+    >
+      <h3 class="text-2xl font-bold">{{ ingredient.strIngredient }}</h3>
+      <p>{{ ingredient.strDescription }}</p>
+    </router-link>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import axiosClient from "../axiosClient";
+const ingredients = ref([]);
+onMounted(() => {
+  axiosClient.get("list.php?i=list").then(({ data }) => {
+    ingredients.value = data.meals;
+  });
+});
+</script>
