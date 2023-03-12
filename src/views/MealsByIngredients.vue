9608a898be7c48f6403a55aeabc6e85e91fa7525
mintsai7777 <king840110@gmail.com>
2023-03-12 21:45:12 +0800

    MealsByIngredients

--- src/views/MealsByIngredients.vue
+++ src/views/MealsByIngredients.vue
@@ -2,15 +2,15 @@
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
     <router-link
-      v-for="ingredient of ingredients"
-      :key="ingredient.id"
-      class="bg-white rounded p-3 mb-3 shadow"
       :to="{
         name: 'byIngredient',
         param: { ingredient: ingredient.idIngredient },
       }"
+      v-for="ingredient of ingredients"
+      :key="ingredient.id"
+      class="block bg-white rounded p-3 mb-3 shadow"
     >
-      <h3 class="text-2xl font-bold">{{ ingredient.strIngredient }}</h3>
+      <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
       <p>{{ ingredient.strDescription }}</p>
     </router-link>
   </div>
