25ff433a4a94492a59e3c454e4cdf2a013958f56
Jack Tsai <jtsai@nexify.com.hk>
2023-03-22 18:02:17 +0800

    padding

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
     <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
   <div
     v-if="meals !== null && !meals.length"
-    class="flex justify-center text-gray-600"
+    class="flex justify-center text-gray-600 px-8"
   >
     There are no meals
   </div>
