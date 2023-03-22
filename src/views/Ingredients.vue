3eccc0af743baad651247c2abde1c79a7281e6fc
Jack Tsai <jtsai@nexify.com.hk>
2023-03-22 17:48:04 +0800

    search function in Ingredients

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="p-8">
     <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full mb-2"
+      placeholder="search by Ingredients"
+      v-model="keyword"
+    />
     <router-link
       :to="{
         name: 'byIngredient',
         params: { ingredient: ingredient.strIngredient },
       }"
-      v-for="ingredient of ingredients"
+      v-for="ingredient of computedIngredients"
       :key="ingredient.id"
       class="block bg-white rounded p-3 mb-3 shadow"
     >
@@ -18,7 +24,20 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import axiosClient from "../axiosClient";
+import { computed } from "@vue/reactivity";
 const ingredients = ref([]);
+const keyword = ref("");
+const computedIngredients = computed(() => {
+  return ingredients.value.filter(
+    (ingredient) =>
+      ingredient.strIngredient
+        .toLowerCase()
+        .includes(keyword.value.toLowerCase()) ||
+      (ingredient.strDescription || "")
+        .toLowerCase()
+        .includes(keyword.value.toLowerCase())
+  );
+});
 onMounted(() => {
   axiosClient.get("list.php?i=list").then(({ data }) => {
     ingredients.value = data.meals;
